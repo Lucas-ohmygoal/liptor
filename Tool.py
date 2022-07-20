8f1063ac1b0c6b594fb3d37c870584ea8e5152f8
Bin Liu <85005419+Lucas-ohmygoal@users.noreply.github.com>
2022-07-20 19:06:17 +0100

    Delete Tool.py

--- Tool.py
+++
@@ -1,9 +0,0 @@
-# Developer : Lucas Liu
-# Date: 6/5/2022 Time: 6:56
-
-class QSSTool:
-    @staticmethod
-    def setQssTool(file_path, obj):
-        with open(file_path, 'r') as f:
-            qss_style = f.read()
-            obj.setStyleSheet(qss_style)
